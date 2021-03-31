ee12e358e0a27cabb57002f1978f3620bd630a49
unknown <harjindersinghmehta@gmail.com>
2021-04-01 00:41:15 +0530

    check login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup , FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+  });
+  this.returnUrl= "/home";
+   }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    this.submitted= true; 
+    // this.loading=true;   
+}
 }
