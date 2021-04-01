d40fda3c40134742bdbe44a113f218c5387283e6
harry-009 <harjaindersinghmetha@gmail.com>
2021-04-01 15:42:17 +0530

    added forgot password functionality

--- src/app/auth/forgotpassword/forgotpassword.component.ts
+++ src/app/auth/forgotpassword/forgotpassword.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
 
 @Component({
-  selector: 'app-forgotpassword',
-  templateUrl: './forgotpassword.component.html',
-  styleUrls: ['./forgotpassword.component.css']
+    selector: 'app-forgotpassword',
+    templateUrl: './forgotpassword.component.html',
+    styleUrls: ['./forgotpassword.component.css']
 })
 export class ForgotpasswordComponent implements OnInit {
 
-  constructor() { }
+    forgotpasswordForm: FormGroup;
+    loading = false;
+    submitted = false;
 
-  ngOnInit(): void {
-  }
+    constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) {
+        this.forgotpasswordForm = this.formBuilder.group({
+            username: ['', [Validators.required, Validators.email]],
+        });
+    }
+
+    ngOnInit(): void {
+    }
+    onSubmit() {
+        this.submitted = true;
+        // this.loading=true;   
+
+        if (!this.forgotpasswordForm.valid) {
+            return;
+        }
+
+        this.authService.forgotPassword(this.forgotpasswordForm.controls.username.value).subscribe(
+            data => {
+                console.log(data);
+                alert("password sent to your email.");
+            },
+            error => {
+                console.log(error);
+                alert("No such user exist.");
+            }
+        );
+        
+    }
 
 }
+
